556cc1b991042e4ccfa49f248cbafd532949a62e
amc95 <adriana.corona@yale.edu>
2016-06-08 14:40:03 -0400

    progress bar

--- src/assets/js/back-to-top.js
+++ src/assets/js/back-to-top.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($){
 		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
 		offset_opacity = 1200,
 		//duration of the top scrolling animation (in ms)
-		scroll_top_duration = 700,
+		scroll_top_duration = 400,
 		//grab the "back to top" link
 		$back_to_top = $('.cd-top');
 
