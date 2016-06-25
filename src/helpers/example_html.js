1b7b8184d9504d32d912953d1e1b3c21bf3851a5
amc95 <adriana.corona@yale.edu>
2016-06-25 17:48:20 -0400

    Complete moving styleguide to panini

--- src/helpers/example_html.js
+++ src/helpers/example_html.js
@@ -1,7 +1,7 @@
 // Allows for the creation of HTML examples and live code in one snippet
 
 module.exports = function(example_html) {
-  var html_code = '```\n' + example_html.fn(this) + '\n```\n\n';
+  var html_code = '```html\n' + example_html.fn(this) + '\n```\n\n';
   var example_output = '<div class="styleguide-code-live">' + example_html.fn(this) + '</div>\n\n';
   return html_code + example_output;
 }
